7ec3edd2f73cd0af4f7eee14e06a61166552826e
Jacob Gad <jacob.gad@hotmail.com>
2022-12-07 19:46:22 +1100

    style: :lipstick: add inter fornt with next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import { Inter } from "@next/font/google";
 
+const inter = Inter({ subsets: ["latin"] });
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 };
