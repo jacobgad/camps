b3709ef05315ccea68cec1aff710f17dbbfc79f0
Jacob Gad <jacob.gad@hotmail.com>
2022-12-25 22:16:38 +1100

    feat: :sparkles: add animation to modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,5 +1,6 @@
 import { Dialog } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { AnimatePresence, motion } from "framer-motion";
 
 type DialogProps = {
   open: boolean;
@@ -11,27 +12,41 @@ type DialogProps = {
 
 export default function Modal(props: DialogProps) {
   return (
-    <Dialog open={props.open} onClose={props.onClose}>
-      <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+    <AnimatePresence>
+      {props.open && (
+        <Dialog
+          open={props.open}
+          onClose={props.onClose}
+          static
+          as={motion.div}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.15 }}
+        >
+          <div className="fixed inset-0 bg-black/50" aria-hidden="true" />
+          <motion.div>
+            <div className="fixed inset-0 py-10 px-4">
+              <Dialog.Panel className="mx-auto flex h-full max-w-sm flex-col rounded-lg bg-gray-50 p-6 shadow-xl">
+                <div className="mb-4 flex justify-between">
+                  <Dialog.Title className="text-lg font-medium">
+                    {props.title}
+                  </Dialog.Title>
+                  <button onClick={props.onClose}>
+                    <XMarkIcon className="h-6 text-gray-400" />
+                  </button>
+                </div>
 
-      <div className="fixed inset-0 py-10 px-4">
-        <Dialog.Panel className="mx-auto flex h-full max-w-sm flex-col rounded-lg bg-gray-50 p-6 shadow-xl">
-          <div className="mb-4 flex justify-between">
-            <Dialog.Title className="text-lg font-medium">
-              {props.title}
-            </Dialog.Title>
-            <button onClick={props.onClose}>
-              <XMarkIcon className="h-6 text-gray-400" />
-            </button>
-          </div>
+                {props.description && (
+                  <Dialog.Description>{props.description}</Dialog.Description>
+                )}
 
-          {props.description && (
-            <Dialog.Description>{props.description}</Dialog.Description>
-          )}
-
-          <div className="flex-grow">{props.children}</div>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+                <div className="flex-grow">{props.children}</div>
+              </Dialog.Panel>
+            </div>
+          </motion.div>
+        </Dialog>
+      )}
+    </AnimatePresence>
   );
 }
