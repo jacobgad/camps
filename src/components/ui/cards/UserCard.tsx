9ad76b085066ffc3d800631f02ebbb123cc8da88
Jacob Gad <jacob.gad@hotmail.com>
2022-12-27 08:25:10 +1100

    style: :lipstick: add active style to cards

--- src/components/ui/cards/UserCard.tsx
+++ src/components/ui/cards/UserCard.tsx
@@ -2,27 +2,18 @@ import { UserIcon } from "@heroicons/react/24/outline";
 
 type UserCardProps = {
   text: string | null;
-  selected?: boolean;
   onClick?: () => void;
 };
 
-export default function UserCard({ text, selected, onClick }: UserCardProps) {
+export default function UserCard({ text, onClick }: UserCardProps) {
   return (
     <button
       onClick={onClick}
-      className={`flex w-full items-center gap-2 rounded-lg border border-gray-300 py-2 px-4 ${
-        selected ? "bg-indigo-500 text-white" : "bg-white"
-      }`}
+      className="flex w-full items-center gap-2 rounded-lg border border-gray-300 bg-white py-2 px-4 active:bg-indigo-50"
     >
-      <UserIcon
-        className={`h-4 stroke-2 ${
-          selected ? "stroke-white" : "stroke-indigo-500"
-        }`}
-      />
+      <UserIcon className="h-4 stroke-indigo-500 stroke-2" />
       <span className="flex-grow text-left text-sm">{text}</span>
-      <span className={`text-sm ${!selected && "text-indigo-500"}`}>
-        {selected ? "Cancel" : "Reallocate"}
-      </span>
+      <span className="text-sm text-indigo-500">Reallocate</span>
     </button>
   );
 }
